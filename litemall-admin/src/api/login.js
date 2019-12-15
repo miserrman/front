1f9675d0a47da4f40364d5d70b9a122a4493cbd2
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-15 22:11:14 +0800

    1

--- litemall-admin/src/api/login.js
+++ litemall-admin/src/api/login.js
@@ -6,7 +6,7 @@ export function loginByUsername(username, password) {
     password
   }
   return request({
-    url: '/admins/login',
+    url: '/auth/login',
     method: 'post',
     data
   })
@@ -14,14 +14,14 @@ export function loginByUsername(username, password) {
 
 export function logout() {
   return request({
-    url: '/admins/logout',
+    url: '/auth/logout',
     method: 'post'
   })
 }
 
 export function getUserInfo(token) {
   return request({
-    url: '/admins/info',
+    url: '/auth/info',
     method: 'get',
     params: { token }
   })
